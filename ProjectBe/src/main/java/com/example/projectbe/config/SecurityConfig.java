82a1c7d036c986b1d8207164fcbceb2e3ca07c6e
ugamezeRepo <ugamezeRepo@gmail.com>
2024-01-31 19:03:56 +0900

    임시 저장

--- ProjectBe/src/main/java/com/example/projectbe/config/SecurityConfig.java
+++ ProjectBe/src/main/java/com/example/projectbe/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,45 +21,28 @@ public class SecurityConfig {
         throws Exception {
         // whiteList
         String[] whiteList = {
-            "/",
-            "/user/loginform",
-            "/user/required_loginform",
-            "/user/login",
+            "/", "/user/loginform", "/user/required_loginform", "/user/login",
         };
 
         httpSecurity
+        	.cors(cors -> cors.and())
             .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(config ->
-                config
-                    .requestMatchers(whiteList)
-                    .permitAll()
-                    .requestMatchers("/admin/**")
-                    .hasRole("ADMIN")
-                    .requestMatchers("/staff/**")
-                    .hasAnyRole("ADMIN", "STAFF")
-                    .anyRequest()
-                    .authenticated()
-            )
-            .formLogin(config ->
-                config
-                    .loginPage("/user/required_loginform")
-                    .loginProcessingUrl("/user/login")
-                    .usernameParameter("userName")
-                    .passwordParameter("password")
-                    .successHandler(new AuthSuccessHandler())
-                    .failureForwardUrl("/user/login_fail")
-                    .permitAll()
-            )
-            .logout(config ->
-                config
-                    .logoutUrl("/user/logout")
-                    .logoutSuccessUrl("/")
-                    .permitAll()
-            )
-            .exceptionHandling(config -> config.accessDeniedPage("/user/denied")
-            )
-            .sessionManagement(config ->
-                config.maximumSessions(1).expiredUrl("/user/expired")
+            .headers(headers -> headers.frameOptions(options -> options.disable()))
+            .authorizeHttpRequests(config -> config
+            		.requestMatchers(whiteList).permitAll()
+                    .requestMatchers("/admin/**").hasRole("ADMIN")
+                    .requestMatchers("/staff/**").hasAnyRole("ADMIN", "STAFF")
+                    .anyRequest().authenticated()
+            ).logout(config -> config
+	                .logoutUrl("/user/logout")
+	                .logoutSuccessUrl("/")
+	                .permitAll()
+            ).exceptionHandling(config -> config
+            		.accessDeniedPage("/user/denied")
+            ).oauth2Login(config -> config
+            		.userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
+            ).sessionManagement(config -> config
+            		.maximumSessions(1).expiredUrl("/user/expired")
             );
 
         return httpSecurity.build();
@@ -83,8 +67,3 @@ public class SecurityConfig {
             .build();
     }
 }
-
-
-
-
-
---
+++ ProjectBe/src/main/java/com/example/projectbe/controller/asd.sql
@@ -0,0 +1,5 @@
+sqlplus sys as sysdba
+비밀번호는 아마 tiger
+create user tmp1 identified by tiger; 
+grant all privileges to tmp1;
+
