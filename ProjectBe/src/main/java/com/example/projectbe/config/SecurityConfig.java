28162f623f035f3ae612e072f482d5f7afd9fe8e
Amuz <52775766+ugameze@users.noreply.github.com>
2024-02-01 18:11:26 +0900

    CSS 적용 허가

--- ProjectBe/src/main/java/com/example/projectbe/config/SecurityConfig.java
+++ ProjectBe/src/main/java/com/example/projectbe/config/SecurityConfig.java
@@ -2,13 +2,8 @@ package com.example.projectbe.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -21,7 +16,7 @@ public class SecurityConfig {
         throws Exception {
         // whiteList
         String[] whiteList = {
-            "/", "/user/loginform", "/user/required_loginform", "/user/login/**",
+            "/", "css/**", "js/**", "img/**"
         };
 
         return httpSecurity
